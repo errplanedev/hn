48250c1f52b492dbbe8701b01108f7db1aa28932
MrCookie <110797323+invixitydev@users.noreply.github.com>
2023-03-05 09:46:22 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# hn
+A simple [Hacker News](https://news.ycombinator.com) client.
+
+## License
+This is licensed under the [GPLv3.0](https://www.gnu.org/licenses/gpl-3.0).
---
+++ script.js
@@ -0,0 +1,30 @@
+let fetchedItems = [];
+let fetchedItemsLength = 45;
+
+async function getPosts() {
+  const list = document.getElementById('posts');
+  const { data: posts } = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+  posts.splice(fetchedItemsLength + 10);
+  posts.forEach(post => {
+    const getMeta = async () => {
+      const { data: postMeta } = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${post}.json`);
+      const li = document.createElement('li');
+      li.innerHTML = `<a href="${postMeta.url}">
+        ${postMeta.title} (${postMeta.url.replace(/^(https?:\/\/[^\/]+).*$/, '$1')})</a>
+        <br>
+        <a href="https://news.ycombinator.com/item?id=${postMeta.id}">
+          <h5>by ${postMeta.by} | ${postMeta.kids.length} comments</h5>
+        </a></br>`;
+      list.appendChild(li);
+    }
+    if(!fetchedItems.includes(post)) {
+      getMeta();
+      fetchedItems.push(post);
+      fetchedItemsLength++;
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', getPosts);
+
+window.onscroll = getPosts;
